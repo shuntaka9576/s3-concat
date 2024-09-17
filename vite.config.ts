8d1a910eff36b082afbb8ac1e7988ec6b5590e95
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2024-09-18 00:35:04 +0900

    fix: exclude 3rd-party libraries from the bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+
+const pkg = JSON.parse(
+  await readFile(new URL('./package.json', import.meta.url), 'utf-8')
+);
 
 export default defineConfig({
   plugins: [
@@ -9,6 +14,9 @@ export default defineConfig({
     }),
   ],
   build: {
+    rollupOptions: {
+      external: Object.keys(pkg.dependencies),
+    },
     lib: {
       entry: resolve(__dirname, './lib/s3-concat.ts'),
       name: 's3-concat',
